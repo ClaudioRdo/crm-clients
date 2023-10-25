207ef81ca97dbf0534fc6c4ac4b94ed1310e8085
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-10-24 23:55:51 -0300

    Error page

---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+import { useRouteError } from 'react-router-dom'
+
+export default function Error() {
+    const error = useRouteError();
+    
+    return(
+        <div className='space-y-8'>
+            <h1 className='text-center text-6xl font-extrabold mt-20 text-blue-900'>CRM - Client</h1>
+            <p className='text-center'>It was an error</p>
+            <p className='text-center'>{error.statusText || error.message}</p>
+        </div>
+    )
+};
---
+++ src/pages/NewClient.jsx
@@ -0,0 +1,3 @@
+import { useNavigate, Form, useActionData, redirect } from 'react-router-dom';
+import Error from '../components/Error';
+import { addClient } from '../data/clients';
